d46357ea235b085d848c9ec4918c400d656ba061
byiringiro-aloys <byiringiroaloys8@gmail.com>
2025-11-18 12:08:26 +0200

    Added userInfo model

--- src/main/java/dev/aloys/jwt_oauth2/jwt_oauth2/Models/userInfoEntity.java
+++ src/main/java/dev/aloys/jwt_oauth2/jwt_oauth2/Models/userInfoEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "USER_INFO")
 public class userInfoEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Id

424c97e8d7e4d03338c4e607be62aa8b28aa0c5e
byiringiro-aloys <byiringiroaloys8@gmail.com>
2025-11-21 13:30:34 +0200

    Configured websecurity for http requests

--- src/main/java/dev/aloys/jwt_oauth2/jwt_oauth2/config/webSecurityConfig.java
+++ src/main/java/dev/aloys/jwt_oauth2/jwt_oauth2/config/webSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -41,13 +42,42 @@ public class webSecurityConfig {
                 .requestMatchers("/api/users/**/profile").hasAnyRole("USER", "ADMIN")
                 .requestMatchers("/api/manager/**").hasAnyRole("MANAGER","ADMIN")
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/api/reports/**").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/api/reports/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )            
+            .csrf(csrf->csrf.disable())
+            .httpBasic(Customizer.withDefaults())
             .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/dashboard"))
             .logout(logout -> logout.logoutSuccessUrl("/login?logout"))
             .build();
     }
 
+    @Order(3)
+    @Bean
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+        .securityMatcher("/dashboard/**","/settings/**","/user/**","/admin/**")
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers("/user/**").hasRole("USER")
+            .requestMatchers("/settings/**").hasAnyRole("USER","ADMIN")
+            .requestMatchers("/dashboard/**").hasAnyRole("USER","ADMIN")
+            .anyRequest().authenticated()
+        )
+        .csrf(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+        .build();
+    }
+
+    @Order(99)
+    @Bean
+    public SecurityFilterChain defaultFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+        .authorizeHttpRequests(auth->auth
+            .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults())
+        .build();
+    }
     
 }
